d04a6a6fb86fcee417c8578737565b982893c3e7
webdev <webdev@webDevVM>
2015-04-13 19:32:20 +0200

    modifications2

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name showcaseApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the showcaseApp
+ */
+angular.module('showcaseApp')
+  .controller('ContactCtrl', function ($scope,$http) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $http.get('images/model.json').
+      success(function(data) {
+        $scope.mail = data.person.mail;
+      })
+  });
